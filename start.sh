d92e7705fb21f8a3f4c0196111cda3344ba2847b
Matthew Mosesohn <mmosesohn@mirantis.com>
2014-04-13 13:53:30 +0400

    log fixes

--- start.sh
+++ start.sh
@@ -5,4 +5,7 @@ puppet apply -v /etc/puppet/modules/nailgun/examples/rabbitmq-only.pp
 service rabbitmq-server stop
 #Just in case stopping service fails
 pkill -u rabbitmq
+mkdir -p /var/log/rabbitmq
+chown -R rabbitmq:rabbitmq /var/log/rabbitmq
+
 /usr/sbin/rabbitmq-server
